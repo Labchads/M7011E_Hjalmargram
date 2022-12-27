a35a2d6900d0c7e28e1a2967363e8ce541c6d3a3
Christoffer Lindkvist <crillemega@gmail.com>
2022-12-27 19:00:17 +0100

    test

--- hjalmargram/hjalmargram-fe/src/components/Header.js
+++ hjalmargram/hjalmargram-fe/src/components/Header.js
@@ -1,17 +1,43 @@
 import React, { Component } from "react";
 import './pages/css/main.css';
+import Login from "./components/pages/Login";
+import Profile from "./components/pages/Profile";
+import Comment from "./components/Comment";
+import axios from 'axios';
+import Post  from "./components/Post";
 
+console.log("ass2");
 class Header extends Component {
   render() {
     return (
         <nav class ="Top">
             <h2>Hjalmargram</h2>
-            <a href="#">Search</a>
-            <a href="#">Notifications</a>
-            <a href="#">Menu</a>
+              <button onClick={(e) => this.navbar(search)}>Search</button>
+              <button onClick={(e) => this.navbar(nots)}>Notifications</button>
+              <button onClick={(e) => this.navbar(login)}>Login</button>
         </nav>
     );
   }
+  navbar(click)
+  {
+      switch(click)
+      {
+        case search:
+          console.log("ass");
+          return <Profile/>;
+        case nots:
+          console.log("ass");
+          return <Post/>;
+        case login:
+          console.log("ass");
+          return <Login/>;
+        default:
+          //todo
+          break;
+      }
+  };
+
+
 }
 
 export default Header;
