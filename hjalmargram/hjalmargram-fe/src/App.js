81934c0a3e0f141eee1a45a328d94fe9d1e99024
Christoffer Lindkvist <crillemega@gmail.com>
2023-01-01 00:29:41 +0100

    Thank god for AI

--- hjalmargram/hjalmargram-fe/src/App.js
+++ hjalmargram/hjalmargram-fe/src/App.js
@@ -1,5 +1,5 @@
-/*import React, { Component, Fragment } from "react";
-import {
+import React, { Component, Fragment, useState } from "react";
+/*import {
   BrowserRouter as Router,
   Switch,
   Route,
@@ -11,24 +11,56 @@ import './main.css';
 import Profile from "./components/pages/Profile";
 */
 
-import Header from "./components/Header";
 import Post  from "./components/Post"
 import Login from "./components/pages/Login";
 import CreateAccount from "./components/pages/CreateAccount";
 
-import { s, n, l} from "./components/Header";
-const bools = [s, n, l];
+
 function App() 
 {
+  const [s, setS] = useState(false);
+  const [n, setN] = useState(false);
+  const [l, setL] = useState(false);
+  function navbar(click)
+  {
+    console.log(click);
+    var a = React.createElement("div", {}, React.createElement("h1", {}, "Disco disco party party"));
+    setS(false);
+    setN(false);
+    setL(false);
+      switch(click)
+      {
+        case 'search':
+        console.log('ass - search');
+        setS(true);
+        break;
+      case 'nots':
+        console.log('ass - nots');
+        setN(true);
+        break;
+      case 'login':
+        console.log('ass - login');
+        setL(true);
+        break;
+      default:
+        break;
+      }
+    };
  return (
   <div>
-    <Header />
-    {bools[0] ? <CreateAccount /> : null}
-    {bools[1] ? <Login /> : null}
-    {bools[2] ? <Post /> : null}
+    <nav class ="Top">
+        <h2>Hjalmargram</h2>
+        <button onClick={(e) => navbar("search")}>Profile</button>
+        <button onClick={(e) => navbar("nots")}>Post</button>
+        <button onClick={(e) => navbar("login")}>Login</button>
+    </nav>
+    {s ? <CreateAccount /> : null}
+    {n ? <Login /> : null}
+    {l ? <Post /> : null}
     <h2>Made it here</h2>
   </div>
   );
 }
+
 
 export default App;
--- hjalmargram/hjalmargram-fe/src/components/Header.jsx
+++ hjalmargram/hjalmargram-fe/src/components/Header.jsx
@@ -7,40 +7,9 @@ export let l = false;
 class Header extends Component 
 {
   render() {
-    return (
-        <nav class ="Top">
-            <h2>Hjalmargram</h2>
-              <button onClick={(e) => this.navbar("search")}>Profile</button>
-              <button onClick={(e) => this.navbar("nots")}>Post</button>
-              <button onClick={(e) => this.navbar("login")}>Login</button>
-        </nav>
-    );
+    return null;
   }
-  navbar(click)
-  {
-    console.log(click);
-    var a = React.createElement("div", {}, React.createElement("h1", {}, "Disco disco party party"));
-    s = false;
-    n = false;
-    l = false;
-      switch(click)
-      {
-        case "search":
-          console.log("ass - search");
-          s = !s;
-          break;
-        case "nots":
-          console.log("ass - nots");
-          n = !n;
-          break;
-        case "login":
-          console.log("ass - login");
-          l = !l;
-          break;
-        default:
-        break;
-      }
-  };
+
 }
 
 export default Header;
