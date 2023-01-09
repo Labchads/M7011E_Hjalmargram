124c76fa5a4108f7ce928ac90dee13fb185c11f6
Crilluz <CrilleMega@gmail.com>
2023-01-09 18:50:52 +0100

    YEA BABY

--- hjalmargram/hjalmargram-fe/src/components/Post.jsx
+++ hjalmargram/hjalmargram-fe/src/components/Post.jsx
@@ -2,17 +2,15 @@ import React, { Component } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import "./pages/css/main.css";
-import PostID from "./postID";
 
-console.log(PostID);
-
+var postID = window.location.pathname.split("/")[2];
 class Post extends Component
 {
   render()
   {
     return(
       <article>
-        <h2>Post ID: <br/><PostID/></h2>
+        <h2>Post ID: <br/>{postID}</h2>
       </article>
     )
   }
